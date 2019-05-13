484c36ce5b651a658da0ba551969ba629ccd7d66
Ayush Maheshwari <ayush.maheshwari@impetus.co.in>
2019-05-13 13:29:07 +0530

    Group id and artifact id change

---
+++ rpacoordinator/src/main/java/com/demo/chatbot/utils/CoordinatorConstants.java
@@ -0,0 +1,11 @@
+package com.demo.chatbot.utils;
+/** The Class RPAConstants. */
+public class CoordinatorConstants {
+
+    /** The Constants defining the various status of the BOT */
+	public static final String STATUS_READY = "READY";
+	public static final String STATUS_RUNNING = "RUNNING";
+    public static final String STATUS_COMPLETED = "COMPLETED";
+    public static final String STATUS_TERMINATED = "TERMINATED";
+
+}
