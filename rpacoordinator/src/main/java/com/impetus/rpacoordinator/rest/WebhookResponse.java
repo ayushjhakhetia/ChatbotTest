a9806cf566f2bbf4249d9f18eb990cc4e5b4e789
Ayush Maheshwari <ayush.maheshwari@impetus.co.in>
2019-05-13 11:28:41 +0530

    rpacoordinator for webhook testing

---
+++ rpacoordinator/src/main/java/com/impetus/rpacoordinator/rest/WebhookResponse.java
@@ -0,0 +1,78 @@
+package com.impetus.rpacoordinator.rest;
+
+import java.util.List;
+
+import com.impetus.rpacoordinator.model.FollowUpEventInput;
+import com.impetus.rpacoordinator.model.FulfillmentMessages;
+import com.impetus.rpacoordinator.model.OutputContexts;
+import com.impetus.rpacoordinator.model.Text;
+
+
+public class WebhookResponse {
+    private String fulfillmentText= " Default fulfillment text";
+    //private List<TextResponse> fulfillmentMessages;
+
+    private String source = "impetus-hook(Default source)";
+    private List<OutputContexts> outputContexts;
+    private List<FulfillmentMessages> fulfillmentMessages;
+    private FollowUpEventInput followUpEventInput;
+
+    public List<FulfillmentMessages> getFulfillmentMessages() {
+		return fulfillmentMessages;
+	}
+
+	public void setFulfillmentMessages(List<FulfillmentMessages> fulfillmentMessages) {
+		this.fulfillmentMessages = fulfillmentMessages;
+	}
+
+	public FollowUpEventInput getFollowUpEventInput() {
+		return followUpEventInput;
+	}
+
+	public void setFollowUpEventInput(FollowUpEventInput followUpEventInput) {
+		this.followUpEventInput = followUpEventInput;
+	}
+
+	public final String getFulfillmentText() {
+        return fulfillmentText;
+    }
+
+    public final void setFulfillmentText(String fulfillmentText) {
+        this.fulfillmentText = fulfillmentText;
+    }
+
+ /*   public final List<TextResponse> getFulfillmentMessages() {
+
+    public final void setFulfillmentMessages(List<Text> fulfillmentMessages) {
+        this.fulfillmentMessages = fulfillmentMessages;
+    }*/
+
+    public final String getSource() {
+        return source;
+    }
+
+    public final void setSource(String source) {
+        this.source = source;
+    }
+
+    public final List<OutputContexts> getOutputContexts() {
+        return outputContexts;
+    }
+
+    public final void setOutputContexts(List<OutputContexts> outputContexts) {
+        this.outputContexts = outputContexts;
+    }
+
+    public WebhookResponse(String speech, List<Text> displayText) {
+        this.fulfillmentText = speech;
+//        this.fulfillmentMessages = displayText;
+    }
+
+    public WebhookResponse(String fulfillmentText) {
+        this.fulfillmentText = fulfillmentText;
+    }
+    public WebhookResponse() {
+        this.fulfillmentText = " I didnt get it, could you repeat(Default Text)";
+    }
+
+}
